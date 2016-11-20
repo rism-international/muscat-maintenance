a14199efbc5e04a7becd1df888ad8cd563f9e96e
stephan <stephan@hirsch-online.com>
2016-11-20 10:05:24 +0100

    Using lambda

--- 20161120-sources-unpublishing.rb
+++ 20161120-sources-unpublishing.rb
@@ -1,13 +1,13 @@
 # encoding: UTF-8
-# ISSUE #1: Setting all records of 456055* to unpublished by request of Bernhard Lutz
+# ISSUE #1: Setting all records of 456055* to unpublished by request of BeLu
 require_relative "lib/maintenance"
 
 collection = Source.where("id like ?", "456055%").where(:wf_stage => 1)
 maintenance = Muscat::Maintenance.new(collection)
 
-process = Proc.new do |record|
+process = lambda { |record|
   record.update(:wf_stage => 'inprogress')
   maintenance.logger.info("#{maintenance.host}: #{record.id} updated to publish.")
-end
+}
 
 maintenance.execute process
--- lib/maintenance.rb
+++ lib/maintenance.rb
@@ -1,6 +1,12 @@
 module Muscat
+  #The maintenance class bundles relevant methods and actions for changing RISM data in Muscat application.
+  #It contains
+  # * Logging
+  # * Progressbar
   class Maintenance
     attr_accessor :collection, :host, :logger, :bar, :filename
+    # Creates a new maintenance object with logging and progressbar
+    # Required parameter: collection of records
     def initialize(collection)
       @filename = File.basename($0, '.rb')
       @host = Socket.gethostname
@@ -9,6 +15,8 @@ module Muscat
       @bar = ProgressBar.new(collection.size)
     end
 
+    # Starts execution of maintenance
+    # Required parameter: lambda or proc defining change routine
     def execute process
       raise 'Process not defined' unless process
       logger.info("#{host}: Size of collection --> #{collection.size}")
