c5c0f49d7ca7565492479f15a164a19ad06e8dcd
stephan <stephan@hirsch-online.com>
2016-11-20 14:33:15 +0100

    Added helper method to yaml

--- 20161120-sources-unpublishing.rb
+++ 20161120-sources-unpublishing.rb
@@ -2,12 +2,12 @@
 # ISSUE #1: Setting all records of 456055* to unpublished by request of BeLu
 require_relative "lib/maintenance"
 
-collection = Source.where("id like ?", "456055%").where(:wf_stage => 1)
-maintenance = Muscat::Maintenance.new(collection)
+sources = Source.where("id like ?", "456055%").where(:wf_stage => 1)
+maintenance = Muscat::Maintenance.new(sources)
 
 process = lambda { |record|
   record.update(:wf_stage => 'inprogress')
-  maintenance.logger.info("#{maintenance.host}: #{record.id} updated to publish.")
+  maintenance.logger.info("#{maintenance.host}: #{record.id} updated to unpublish.")
 }
 
 maintenance.execute process
--- lib/maintenance.rb
+++ lib/maintenance.rb
@@ -4,14 +4,13 @@ module Muscat
   # * Logging
   # * Progressbar
   class Maintenance
-    attr_accessor :collection, :host, :logger, :bar, :filename
+    attr_accessor :collection, :host, :logger, :bar
     # Creates a new maintenance object with logging and progressbar
     # Required parameter: collection of records
     def initialize(collection)
-      @filename = File.basename($0, '.rb')
-      @host = Socket.gethostname
-      @logger = Logger.new("#{File.dirname($0)}/log/#{@filename}.log")
       @collection = collection
+      @host = Socket.gethostname
+      @logger = Logger.new("#{File.dirname($0)}/log/#{File.basename($0, '.rb')}.log")
       @bar = ProgressBar.new(collection.size)
     end
 
@@ -24,6 +23,11 @@ module Muscat
         process.call(e)
         bar.increment!
       end
+    end
+
+    # Helper to parse related yaml-file
+    def self.yaml
+      YAML.load_file("#{File.dirname($0)}/#{File.basename($0, '.rb')}.yml") rescue nil
     end
   end
 end
