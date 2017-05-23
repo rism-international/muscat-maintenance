cb870c659974d801d7c022e24f2f7658ab26259d
stephan <stephan@hirsch-online.com>
2017-05-23 07:44:14 +0200

    Fixed 594c

--- 20170514-sources-594c.rb
+++ 20170514-sources-594c.rb
@@ -27,15 +27,32 @@ maintenance = Muscat::Maintenance.new(sources)
 
 process = lambda { |record|
   modified = false
-  instrument = ""
+  n594b = ""
+  n594c = ""
+  result = ""
   marc = record.marc
   marc.each_by_tag("594") do |n|
     c = n.fetch_first_by_tag("c") rescue nil
     if c && c.content =~ /org|orch|cemb/
+      n594c = c.content
       b = n.fetch_first_by_tag("b")
       if b && b.content
-        b.content += (c.content.start_with?("(") && c.content.end_with?(")")) ? "#{c.content}" : " (#{c.content})"
-        instrument = c.content
+        n594b = b.content
+        if c.content.end_with?(")") 
+          if c.content.start_with?("(")
+            b.content += " #{c.content}"
+            result = b.content
+          else
+            if c.content.start_with?(" #{b.content}")
+              instrument = c.content.gsub(/.*\s+\(/," (")
+              b.content += "#{instrument}"
+              result = b.content
+            end
+          end
+        else
+          b.content += " (#{c.content})"
+          result = b.content
+        end
         c.content = "1"
         modified = true
       end
@@ -43,7 +60,7 @@ process = lambda { |record|
   end
   if modified
     record.save
-    maintenance.logger.info("#{maintenance.host}: Source ##{record.id} $594c #{instrument} appended to 594b.")
+    maintenance.logger.info("#{maintenance.host}: Source ##{record.id} 594: '#{n594b.yellow}' + '#{n594c.yellow}' => '#{result.green}'")
   end
 }
 
