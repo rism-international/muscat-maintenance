3356aafaf10ab919deba26f1772a36a616405620
stephan <stephan@hirsch-online.com>
2017-09-07 16:23:15 +0200

    Upd generate

--- export/generate_beacon.rb
+++ export/generate_beacon.rb
@@ -9,24 +9,20 @@
 #./r2rml-parser.sh -p r2rml.properties
 require 'fileutils'
 
-current = File.expand_path(__FILE__) + "/"
-#prefix = current + "../../beacon/muscat/"
-prefix = current
-#prefix = "/home/dev/projects/beacon/"
-puts current
-#puts File.expand_path(prefix) + "/"
+current = File.expand_path(File.dirname($0)) + "/"
+prefix = File.expand_path(current + "../../../") + "/"
 
 cfile = "/tmp/r2rml.properties"
-#File.delete(cfile) if File.exist?(cfile)
-#FileUtils.cp(prefix + "muscat/vendor/rdf/conf/r2rml.properties", cfile)
-#f = File.open(cfile, "a")
-#f.write("db.login=#{ENV["BEACON_USER"]}\n")
-#f.write("db.password=#{ENV["BEACON_PW"]}\n")
-#f.write("db.url=jdbc:mysql://127.0.0.1:3306/#{ENV["BEACON_DB"]}\n")
-#f.write("mapping.file=../conf/mappings/person.ttl")
-#f.close
+File.delete(cfile) if File.exist?(cfile)
+FileUtils.cp(prefix + "vendor/rdf/conf/r2rml.properties", cfile)
+f = File.open(cfile, "a")
+f.write("db.login=#{ENV["BEACON_USER"]}\n")
+f.write("db.password=#{ENV["BEACON_PW"]}\n")
+f.write("db.url=jdbc:mysql://127.0.0.1:3306/#{ENV["BEACON_DB"]}\n")
+f.write("mapping.file=#{prefix}/vendor/rdf/conf/mappings/person2.ttl\n")
+f.close
 
-
+%x( #{prefix + "vendor/rdf/bin/r2rml-parser.sh -p /tmp/r2rml.properties"} )
 
 #2 import pe.ttl to jena
 #3 build beacon file from RISM::RDF:Beacon
