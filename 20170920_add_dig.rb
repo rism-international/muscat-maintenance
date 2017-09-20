18d838e67f4f42a0004f4082ae03b1be4935ae9e
stephan <stephan@hirsch-online.com>
2017-09-20 09:16:33 +0200

    Updated doc

--- 20170920_add_dig.rb
+++ 20170920_add_dig.rb
@@ -1,7 +1,7 @@
 # encoding: UTF-8
 puts "##################################################################################################"
-puts "###########################  ISSUE #28: Add digitalization links #################################"
-puts "############################   Expected collection size: 620  ####################################"
+puts "#######################  ISSUE: Add digitalization links of B-Br  ################################"
+puts "############################   Expected collection size: 1.348  ##################################"
 puts "##################################################################################################"
 puts ""
 
@@ -15,7 +15,7 @@ yaml.each do |e|
   url = e[e.keys.first]
   ref_a1 = e.keys.first
   sx = Source.solr_search do fulltext ref_a1, :fields => '035a' end
-  #puts "this is missing: #{ref_a1}!!" if sx.total == 0
+  maintenance.logger.info("#{maintenance.host}: Source ##{s.id} has no holding by B-Br '#{url}'") if sx.total == 0
   sx.results.each do |s|
     a1 = s.marc.first_occurance("035", "a").content.split(";").first
     if a1 == ref_a1
@@ -32,42 +32,9 @@ yaml.each do |e|
           h.save if modified
         end
       end
-      #puts "#{ref_a1} == #{a1}"
       cnt+=1
     else
       next
-      
     end
   end
-
 end
-puts cnt
-
-=begin
-
-sources = Source.where(:id => yaml.keys)
-
-maintenance = Muscat::Maintenance.new(sources)
-
-process = lambda { |record|
-  modified = false
-  url = yaml[record.id][:url]
-  bib = yaml[record.id][:bib]
-  object = record.holdings.empty? ? record : record.holdings.where(:lib_siglum => bib).take  
-  if !object
-    maintenance.logger.info("#{maintenance.host}: Source ##{record.id} has no holding from '#{bib}'")
-  else
-    marc = object.marc
-    new_856 = MarcNode.new(Source, "856", "", "4#")
-    ip = marc.get_insert_position("856")
-    new_856.add(MarcNode.new(Source, "u", "#{url}", nil))
-    new_856.add(MarcNode.new(Source, "z", "[digitized version]", nil))
-    marc.root.children.insert(ip, new_856)
-    maintenance.logger.info("#{maintenance.host}: Source ##{record.id} #{bib} new digitizalization with content '#{url}'")
-    modified = true
-    object.save if modified
-  end
-}
-
-maintenance.execute process
-=end
