bdba26421fdff9ea5c39aa425b46cb2830d09193
stephan <stephan@hirsch-online.com>
2017-09-20 09:28:54 +0200

    Added progressbar to add digi

--- 20170920_add_dig.rb
+++ 20170920_add_dig.rb
@@ -9,13 +9,13 @@ require_relative "lib/maintenance"
 
 yaml = Muscat::Maintenance.yaml
 maintenance = Muscat::Maintenance.new(yaml)
-cnt=0
+bar = ProgressBar.new(1348)
 
 yaml.each do |e|
   url = e[e.keys.first]
   ref_a1 = e.keys.first
   sx = Source.solr_search do fulltext ref_a1, :fields => '035a' end
-  maintenance.logger.info("#{maintenance.host}: Source ##{s.id} has no holding by B-Br '#{url}'") if sx.total == 0
+  maintenance.logger.info("#{maintenance.host}: Source A/I '#{ref_a1}' has no holding by B-Br!") if sx.total == 0
   sx.results.each do |s|
     a1 = s.marc.first_occurance("035", "a").content.split(";").first
     if a1 == ref_a1
@@ -30,9 +30,9 @@ yaml.each do |e|
           maintenance.logger.info("#{maintenance.host}: Source ##{s.id} B-Br new digitizalization with content '#{url}'")
           modified = true
           h.save if modified
+          bar.increment!
         end
       end
-      cnt+=1
     else
       next
     end
