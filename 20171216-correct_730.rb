d957810b7b6f194be98cfa2ff7e78f5374ba1fb8
stephan <stephan@hirsch-online.com>
2017-12-18 11:26:48 +0100

    Updated 730 correction

---
+++ 20171216-correct_730.rb
@@ -0,0 +1,101 @@
+# encoding: UTF-8
+puts "##################################################################################################"
+puts "###################### ISSUE Fix 730 in case of any punctuation     ##############################"
+puts "#####################   Expected collection size: 10.046  ########################################"
+puts "##################################################################################################"
+puts ""
+
+require_relative "lib/maintenance"
+
+yaml = Muscat::Maintenance.yaml
+sources = Source.where(:id => yaml.keys)
+maintenance = Muscat::Maintenance.new(sources)
+
+puts sources.size
+process = lambda { |record|
+  modified = false
+  marc = record.marc
+  new_texts = yaml[ "%09d" % record.id.to_s ]
+  post_semicolon = ""
+  if record.id > 456082230 and record.id < 457000000
+    rda = true
+  else
+    rda = false
+  end
+  marc.each_by_tag("730") do |tag|
+    if rda
+      g_tag = tag.fetch_first_by_tag("g")
+      g_tag.content = "RDA"
+      binding.pry
+    end
+    existing_tag = tag.deep_copy
+    a_tag = tag.fetch_first_by_tag("a")
+    #puts "#{a_tag.content} <<-->> #{new_texts.inspect}"
+    next if new_texts.include?(a_tag.content) || !a_tag.content
+
+    k_tag = tag.fetch_first_by_tag("k")
+    o_tag = tag.fetch_first_by_tag("o")
+   
+    if o_tag && o_tag.content
+      existing_text = "#{a_tag.content}. #{o_tag.content}"
+      new_texts.each do |t|
+        if t == existing_text
+          tag.fetch_first_by_tag("0").destroy_yourself rescue nil
+          a_tag.destroy_yourself
+          o_tag.destroy_yourself
+          tag.add(MarcNode.new(Source, "a", "#{t}", nil))
+          tag.sort_alphabetically
+          next
+        end
+      end
+    elsif k_tag && k_tag.content
+      next if k_tag.content == "Insertions"
+      existing_text = "#{a_tag.content}. #{k_tag.content}"
+      new_texts.each do |t|
+        if t == existing_text
+          tag.fetch_first_by_tag("0").destroy_yourself rescue nil
+          a_tag.destroy_yourself
+          k_tag.destroy_yourself
+          tag.add(MarcNode.new(Source, "a", "#{t}", nil))
+          tag.sort_alphabetically
+          next
+        end
+      end
+    elsif a_tag
+      if a_tag.content == post_semicolon
+        tag.destroy_yourself
+      end
+      existing_text = "#{a_tag.content}".gsub("[", "<").gsub("]", ">")
+      new_texts.each do |t|
+        if t.include?(";") && t.start_with?(existing_text)
+          tag.fetch_first_by_tag("0").destroy_yourself rescue nil
+          a_tag.destroy_yourself
+          tag.add(MarcNode.new(Source, "a", "#{t}", nil))
+          tag.sort_alphabetically
+          maintenance.logger.info("#{maintenance.host}: #{record.id}: #{existing_tag.to_s.strip} --> #{tag}")
+          post_semicolon = t.split(";").last.strip
+          next
+        end
+        if t == existing_text
+          tag.fetch_first_by_tag("0").destroy_yourself rescue nil
+          a_tag.destroy_yourself
+          tag.add(MarcNode.new(Source, "a", "#{t}", nil))
+          tag.sort_alphabetically
+          next
+        end
+      end
+    else
+      puts "WTF"
+      binding.pry
+    end
+    if existing_tag.to_s != tag.to_s
+      maintenance.logger.info("#{maintenance.host}: #{record.id}: #{existing_tag.to_s.strip} --> #{tag}")
+    end
+#        modified = true
+#    end
+  end
+#  record.save if modified
+#  maintenance.logger.info("#{maintenance.host}: Source ##{record.id} $710$4 'asn' changed to 'oth'.")
+}
+
+maintenance.execute process
