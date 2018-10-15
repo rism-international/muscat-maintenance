298b87b9f6bf8c6ec2fa3bc7bb943d38e950a0b9
stephan <stephan@hirsch-online.com>
2018-10-15 11:00:57 +0200

    Adding replacing 'print' term

---
+++ 20181015-replace-print.rb
@@ -0,0 +1,29 @@
+# encoding: UTF-8
+puts "##################################################################################################"
+puts "#############################  ISSUE : Replace 'print'          ##################################"
+puts "#######################   Expected collection size: c.100000     #################################"
+puts "##################################################################################################"
+puts ""
+
+require_relative "lib/maintenance"
+
+sources = Source.find_by_sql("SELECT * FROM sources s where marc_source REGEXP '=593[^\n]*\[[.$.]]a[Pp]rint'")
+
+maintenance = Muscat::Maintenance.new(sources)
+process = lambda { |record|
+  modified = false
+  PaperTrail.request.disable_model(Source)
+  s.marc.each_by_tag("593") do |t|
+    t.each_by_tag("a") do |tn|
+      next if !(tn && tn.content)
+      if tn.content == "print"
+        tn.content = "Print"
+        modified = true
+      end
+    end
+  end
+  record.save if modified rescue next
+}
+
+maintenance.execute process
+
