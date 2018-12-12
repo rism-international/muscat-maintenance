3777546a87a624427ea0b866c4185c32a5d80e24
stephan <stephan@hirsch-online.com>
2018-12-12 22:01:41 +0100

    Adding log to move 597

--- 20181120-move_597.rb
+++ 20181120-move_597.rb
@@ -5,44 +5,49 @@ puts "#######################   Expected collection size: c.96.330     #########
 puts "##################################################################################################"
 puts ""
 
-require 'progress_bar'
+require_relative "lib/maintenance"
 
-sx = Source.find_by_sql("SELECT * FROM sources s where marc_source REGEXP '=597[^\n]*\[[.$.]]a'")
-pb = ProgressBar.new(sx.size)
+sources = Source.find_by_sql("SELECT * FROM sources s where marc_source REGEXP '=597[^\n]*\[[.$.]]a'")
 PaperTrail.request.disable_model(Source)
-sx.each do |s|
-  impressum = ""
+maintenance = Muscat::Maintenance.new(sources)
+
+
+process = lambda { |source|
+  impressum = []
   modified = false
+  node_597 = []
   begin
-    s.marc.each_by_tag("597") do |t|
+    source.marc.each_by_tag("597") do |t|
+      node_597 << t
       t.each_by_tag("a") do |tn|
-        impressum = tn.content
-        tn.destroy_yourself
+        impressum << tn.content
       end
-      t.each_by_tag("8") do |tn|
-        if tn.content == '01'
-          modified = true
-          t.destroy_yourself
-        end
-      end
+      #t.each_by_tag("8") do |tn|
+      #  if tn.content == '01'
+      #    modified = true
+      #  end
+      #end
     end
 
-    s.marc.each_by_tag("245") do |t|
+    source.marc.each_by_tag("245") do |t|
       t.each_by_tag("a") do |tn|
-        if modified
-          tn.content = "#{tn.content} [#{impressum}]"
-        end
+        tn.content = "#{tn.content} [#{impressum.join("; ")}]"
+        modified = true
       end
     end
-
+    node_597.each do |node| 
+      node.destroy_yourself
+    end
+    
   rescue 
     next
   end
-  s.suppress_reindex
+  source.suppress_reindex
   PaperTrail.request(enabled: false) do
-    s.save if modified
+    source.save if modified
   end
-  s = nil
-  pb.increment!
-end
+  maintenance.logger.info("#{maintenance.host}: Source ##{source.id} moved 597 to 245") if modified
+  source = nil
+}
 
+maintenance.execute process
