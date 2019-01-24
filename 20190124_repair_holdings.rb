6b0d835589417b66eab067070b1056ce9923d340
stephan <stephan@hirsch-online.com>
2019-01-24 11:51:57 +0100

    Fix holdings with orphaned nodes

---
+++ 20190124_repair_holdings.rb
@@ -0,0 +1,32 @@
+# encoding: UTF-8
+puts "##################################################################################################"
+puts "##########    ISSUE #: Repair holdings with orphaned nodes  ######################################"
+puts "############                Expected size: ca. 61                    #############################"
+puts "##################################################################################################"
+puts " "
+
+require_relative "lib/maintenance"
+
+yaml = Muscat::Maintenance.yaml
+hx = Holding.where(id: yaml.keys)
+maintenance = Muscat::Maintenance.new(hx)
+
+process = lambda { |record|
+  modified = false
+  n = yaml[record.id]
+  m = MarcHolding.new(record.marc_source)
+  m.load_source(false)
+  m.each_by_tag(n.keys.first) do |node|
+    if node.to_s.include?(n.values.first)
+      node.destroy_yourself
+      modified = true
+    end
+  end
+  if modified
+    record.marc = m
+    record.save
+  end
+  maintenance.logger.info("#{maintenance.host}: Holding ##{record.id} fixed orphaned node")
+}
+
+maintenance.execute process
