a989fe8ce578df5067b67c6fff8311ec88335f30
stephan <stephan@hirsch-online.com>
2019-04-05 10:21:15 +0200

    Added Eichstädt digitalization links

---
+++ 20190328-eich.rb
@@ -0,0 +1,31 @@
+# encoding: UTF-8
+puts "##################################################################################################"
+puts "################    ISSUE tasks: Add digitalization links from Eichstätt   #######################"
+puts "############################   Expected collection size: 2.300  ##################################"
+puts "##################################################################################################"
+puts ""
+
+require_relative "lib/maintenance"
+
+yaml = Muscat::Maintenance.yaml
+
+sources = Source.where(:id => yaml.keys)
+
+maintenance = Muscat::Maintenance.new(sources)
+
+process = lambda { |record|
+modified = false
+url = yaml[record.id]
+marc = record.marc
+new_856 = MarcNode.new(Source, "856", "", "4#")
+ip = marc.get_insert_position("856")
+new_856.add(MarcNode.new(Source, "u", "#{url}", nil))
+new_856.add(MarcNode.new(Source, "x", "Digitalization", nil))
+new_856.sort_alphabetically
+marc.root.children.insert(ip, new_856)
+maintenance.logger.info("#{maintenance.host}: Source ##{record.id} new digitizalization with content '#{url}'")
+modified = true
+record.save if modified
+}
+
+maintenance.execute process
