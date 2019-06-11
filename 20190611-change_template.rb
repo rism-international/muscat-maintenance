b0989ddb9d38f8f50c661aae189279fb83245632
stephan <stephan@hirsch-online.com>
2019-06-11 12:16:29 +0200

    Change template with folder records of D-Dl

---
+++ 20190611-change_template.rb
@@ -0,0 +1,20 @@
+# encoding: UTF-8
+puts "##################################################################################################"
+puts "################  ISSUE: Change template with folder items from slub       #######################"
+puts "##########################   Expected collection size: ca.50    ##################################"
+puts "##################################################################################################"
+puts ""
+
+require_relative "lib/maintenance"
+
+
+sources = Folder.find(289).folder_items.map{|e| e.item}
+maintenance = Muscat::Maintenance.new(sources)
+
+process = lambda { |record|
+  record.change_template_to(1)
+  #record.update(record_type: 11)
+  maintenance.logger.info("#{maintenance.host}: Source ##{record.id} switched to collection template")
+}
+
+maintenance.execute process
