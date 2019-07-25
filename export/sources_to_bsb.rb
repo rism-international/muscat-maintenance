a1916dc156ed4ae5834fa3b4bb30a56f71d8ee8d
stephan <stephan@hirsch-online.com>
2019-07-25 09:12:27 +0200

    Adding 785 followup prints to BSB export

--- export/sources_to_bsb.rb
+++ export/sources_to_bsb.rb
@@ -21,9 +21,11 @@ bar = ProgressBar.new(sources.size)
 
 sources.each do |s|
   record = Source.find(s) rescue next
-
+  ###################################################################
+  #######          Custom local changes for BSB             #########
+  ###################################################################
+  #
   # FIX to include holding.source_id at $o and description at $a with composite
-  #######################################################################
   if record.record_type == 11
     record.marc.each_by_tag("774") do |node|
       holding_node = node.fetch_first_by_tag("4")
@@ -37,6 +39,23 @@ sources.each do |s|
         source_id = node.fetch_first_by_tag("w").content rescue nil
         source = Source.find(source_id)
         node.add(MarcNode.new(Source, "a", source.name, nil))
+      end
+    end
+  end
+
+  # FIX to display references to folloup prints in 785 (as with 774)
+  if record.record_type == 8
+    record.referring_sources.each do |followup_print|
+      followup_print.marc.each_by_tag("775") do |node|
+        ref_id = node.fetch_first_by_tag("w").content rescue nil
+        if ref_id and ref_id == record.id
+          new_785 = MarcNode.new(Source, "785", "", "10")
+          ip = record.marc.get_insert_position("785")
+          new_785.add(MarcNode.new(Source, "a", followup_print.name, nil))
+          new_785.add(MarcNode.new(Source, "w", followup_print.id, nil))
+          record.marc.root.children.insert(ip, new_785)
+          binding.pry
+        end
       end
     end
   end
