7076e3e0c6f6f7df4e23af0622cb633b8c12743d
stephan <stephan@hirsch-online.com>
2019-10-24 10:30:33 +0200

    StandardTerms with translation as json

--- report/2019-10-24_stdterm.rb
+++ report/2019-10-24_stdterm.rb
@@ -1,7 +1,7 @@
-ofile = "#{Rails.root}/housekeeping/maintenance/report/2019-10-24_stdterm.yml"
-res = {}
+ofile = "#{Rails.root}/housekeeping/maintenance/report/2019-10-24_stdterm.json"
+res = []
 StandardTerm.all.order(:term).each do |standard_term|
-  res[standard_term.term] = standard_term.alternate_terms.gsub(/[\n\r]/, "; ").gsub("; ; ", "; ") unless standard_term.alternate_terms.blank?
+  res << {standard_term.term =>  standard_term.alternate_terms.gsub(/[\n\r]/, "; ").gsub("; ; ", "; ")} unless standard_term.alternate_terms.blank?
 end
-File.write(ofile, res.to_yaml)
+File.write(ofile, res.to_json)
 
