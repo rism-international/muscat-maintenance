716545fc00c7145cb6bc1928b1e88bdbb6dca422
stephan <stephan@hirsch-online.com>
2019-11-22 08:51:53 +0100

    Added PL departments

---
+++ 20191121-add_department.rb
@@ -0,0 +1,27 @@
+# encoding: UTF-8
+puts "##################################################################################################"
+puts "####################  ISSUE: Add department in 852$b              ################################"
+puts "##########################   Expected collection size: 1.800    ##################################"
+puts "##################################################################################################"
+puts ""
+
+require_relative "lib/maintenance"
+
+records = Source.where(lib_siglum: "PL-Wmfc") + Holding.where(lib_siglum: "PL-Wmfc")
+maintenance = Muscat::Maintenance.new(records)
+
+process = lambda { |record|
+  modified = false
+  record.marc.each_by_tag("852") do |tag|
+    next if tag.fetch_first_by_tag("b")
+    tag.add(MarcNode.new(Source, "b", "Muzeum Fryderyka Chopina", nil))
+    tag.sort_alphabetically
+    modified = true
+  end
+  if modified
+    maintenance.logger.info("#{maintenance.host}: Record ##{record.class.to_s} added $b Museum ...") if modified
+    record.save
+  end
+}
+
+maintenance.execute process
