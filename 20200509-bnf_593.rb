6c3e1fc787b93e32a7058cf224b407bb8dcd0554
stephan <stephan@hirsch-online.com>
2020-05-15 09:23:43 +0200

    Replacing 593 values

--- 20200509-bnf_593.rb
+++ 20200509-bnf_593.rb
@@ -1,13 +1,13 @@
 # encoding: UTF-8
 puts "##################################################################################################"
-puts "################    ISSUE tasks: Cleanup 300a values                       #######################"
+puts "################    ISSUE tasks: Cleanup 593  values                       #######################"
 puts "############################   Expected collection size: 10.000 ##################################"
 puts "##################################################################################################"
 puts ""
 
 require_relative "lib/maintenance"
 
-terms = YAML.load_file("#{Rails.root}/housekeeping/maintenance/20200508-bnf_300a.yml")
+terms = YAML.load_file("#{Rails.root}/housekeeping/maintenance/20200509-bnf_593.yml")
 
 def replace_all(str, hash)
   new_str = str.dup
@@ -21,13 +21,13 @@ maintenance = Muscat::Maintenance.new(sources)
 process = lambda { |record|
   modified = false
   marc = record.marc
-  marc.each_by_tag("300") do |n|
+  marc.each_by_tag("593") do |n|
     n.each_by_tag("a") do |sf|
       old_content = sf.content
       new_content = replace_all(sf.content,terms)
       if new_content != old_content
         modified = true
-        maintenance.logger.info("#{maintenance.host}: Source ##{record.id} 300a: '#{old_content.yellow}' => '#{new_content.green}'")
+        maintenance.logger.info("#{maintenance.host}: Source ##{record.id} 593: '#{old_content.yellow}' => '#{new_content.green}'")
         sf.content = new_content
       end
     end
