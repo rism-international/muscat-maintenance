84b1744d9dd41569208194cdd2d67a55700f33ab
stephan <stephan@hirsch-online.com>
2020-08-27 13:12:44 +0200

    Replace funtion with ICCU 710

--- 20200824-replace_function.rb
+++ 20200824-replace_function.rb
@@ -11,13 +11,13 @@ require_relative "lib/maintenance"
 yaml = HashWithIndifferentAccess.new(YAML.load_file("#{File.dirname($0)}/#{File.basename($0, '.rb')}.yml")) rescue nil
 PACKETS = 10000
 host = Socket.gethostname
-size = Source.find_by_sql("SELECT * FROM sources where wf_owner=268 and wf_stage = 0 and marc_source like '%=700%'").size
+size = Source.find_by_sql("SELECT * FROM sources where wf_owner=268 and wf_stage = 0 and marc_source like '%=710%'").size
 logger = Logger.new("#{File.dirname($0)}/log/#{File.basename($0, '.rb')}.log")
 bar = ProgressBar.new(size)
 
 (0..(size / PACKETS )).each do |packet|
   GC.start
-  sources = Source.find_by_sql("SELECT * FROM sources where wf_owner=268 and wf_stage = 0 and marc_source like '%=700%' order by id LIMIT #{PACKETS} offset #{PACKETS * packet}")
+  sources = Source.find_by_sql("SELECT * FROM sources where wf_owner=268 and wf_stage = 0 and marc_source like '%=710%' order by id LIMIT #{PACKETS} offset #{PACKETS * packet}")
   PaperTrail.request.disable_model(Source)
 
   sources.each do |record| 
@@ -25,7 +25,7 @@ bar = ProgressBar.new(size)
     record.suppress_reindex
     bar.increment!
 
-    record.marc.by_tags("700").each do |n|
+    record.marc.by_tags("710").each do |n|
       n.each_by_tag("4") do |sf|
         if yaml.include?(sf.content)
           sf.content = yaml[sf.content]
@@ -35,7 +35,7 @@ bar = ProgressBar.new(size)
     end
     if modified
       record.save#! rescue next
-      logger.info("#{host}: ICCU #{record.id} change 700$4 function")
+      logger.info("#{host}: ICCU #{record.id} change 710$4 function")
     end
   end
 end
