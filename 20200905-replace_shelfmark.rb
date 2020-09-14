6e810a0a09e1691411777c3f4551d7217e669530
stephan <stephan@hirsch-online.com>
2020-09-14 13:37:58 +0200

    Cleaning up shelfmark with BSB

---
+++ 20200905-replace_shelfmark.rb
@@ -0,0 +1,39 @@
+# encoding: UTF-8
+#
+puts "##################################################################################################"
+puts "####################    ISSUE: Change shelfmark with BSB holding        ##########################"
+puts "#########################   Expected collection size: ca. 680   ##################################"
+puts "##################################################################################################"
+puts ""
+
+require_relative "lib/maintenance"
+
+sx = Source.where(lib_siglum: 'D-Mbs')
+hx = Holding.where(lib_siglum: 'D-Mbs')
+sources = sx + hx
+maintenance = Muscat::Maintenance.new(sources)
+
+process = lambda { |record|
+  id = record.source_id ? record.source_id : record.id
+  modified = false
+  marc = record.marc
+  old_content = ""
+  new_content = ""
+  marc.each_by_tag("852") do |tag|
+    tag.each_by_tag("c") do |sf|
+      if sf.content =~ /Mus.\s{0,1}pr/
+        old_content = sf.content
+        new_content = sf.content.gsub("Mus. pr.", "Mus.pr.").gsub(/Mus\.pr\.([0-9])/, 'Mus.pr. \1' )
+        sf.content = new_content
+        modified = true
+      end
+    end
+  end
+  
+  if modified
+    maintenance.logger.info("#{maintenance.host}: #{record.class} #{id} changed shelfmark '#{old_content}' to '#{new_content}'" ) if old_content != new_content
+    #ecord.save #rescue next
+  end
+}
+
+maintenance.execute process
--- util/oai_harvest.rb
+++ util/oai_harvest.rb
@@ -1,18 +1,21 @@
 require 'oai'
 #url = "https://opus4.kobv.de/opus4-udk/oai?verb=GetRecord&metadataPrefix=oai_dc&identifier=oai:kobv.de-opus4-udk:78"
-url = "https://opus4.kobv.de/opus4-udk/oai"
-client = OAI::Client.new url, :headers => { "identifier" => "oai:kobv.de-opus4-udk:78" }
+#url = "https://opus4.kobv.de/opus4-udk/oai"
+url = "https://eu02.alma.exlibrisgroup.com/view/oai/43ACC_ONB/request?verb=ListRecords&metadataPrefix=marc21&set=MUSHANMARC"
+#url = "https://eu02.alma.exlibrisgroup.com/view/oai/43ACC_ONB/request?"
+client = OAI::Client.new url# :headers => { "identifier" => "oai:alma.ACCONB:9958492376403338", "set" => "MUSHANMARC", "metadataPrefix" => "marc21" }
+#client = OAI::Client.new url, :headers => { "identifier" => "oai:kobv.de-opus4-udk:78" }
   response = client.list_records
   # Get the first page of records
   response.each do |record| 
          puts record.metadata
   end
-  #           # Get the second page of records
-  #             response = client.list_records(:resumption_token => response.resumption_token)
-  #               response.each do |record|
-  #                   puts record.metadata
-  #                     end
-  #                       # Get all pages together (may take a *very* long time to complete)
-  #                         client.list_records.full.each do |record|
-  #                             puts record.metadata
-  #                               end
+  #et the second page of records
+# response = client.list_records(:resumption_token => response.resumption_token)
+# response.each do |record|
+#   puts record.metadata
+# end
+  # Get all pages together (may take a *very* long time to complete)
+# client.list_records.full.each do |record|
+#   puts record.metadata
+# end
