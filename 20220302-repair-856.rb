121728f952e743f88d9d7d2a67b763635f164c27
stephan <stephan@hirsch-online.com>
2022-03-04 13:25:07 +0100

    Passing exiting  nodes

--- 20220302-repair-856.rb
+++ 20220302-repair-856.rb
@@ -17,32 +17,19 @@ regexp = Regexp.union(terms)
 
 process = lambda { |record|
   modified = false
-  changed = ""
-  old_content = ""
   record.marc.each_by_tag("856") do |tag|
     tag_x = tag.fetch_first_by_tag("x") 
     tag_z = tag.fetch_first_by_tag("z")
 
+    next if tag_x
     next unless tag_z
     next unless regexp.match(tag_z.content)
 
-    if tag_x 
-      if tag_x.content != "Other"
-        next
-      else
-        old_content = tag_x.content
-        tag_x.content = "Digitized"
-        modified = true
-        changed = "changed from '$x#{old_content}' to"
-      end
-    else
-      tag.add(MarcNode.new(record.class, "x", "Digitized", nil))
-      tag.sort_alphabetically
-      modified = true
-      changed = "created"
-    end
+    tag.add(MarcNode.new(record.class, "x", "Digitized", nil))
+    tag.sort_alphabetically
+    modified = true
 
-    maintenance.logger.info("#{maintenance.host}: #{record.class} ##{record.id} '$z#{tag_z.content}' #{changed} '$xDigitized'") if modified
+    maintenance.logger.info("#{maintenance.host}: #{record.class} ##{record.id} '$z#{tag_z.content}' created '$xDigitized'") if modified
   
   end
 
