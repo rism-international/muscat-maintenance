b5587b29654bad7a61d74d0e1a6a62078e96d41b
stephan <stephan@hirsch-online.com>
2022-06-21 14:16:53 +0200

    Make holdings list uniq

--- 2022-06-02_add-holding-indicator.rb
+++ 2022-06-02_add-holding-indicator.rb
@@ -19,28 +19,38 @@ data.each do |e|
             '856u' => e[5], '856x' => e[6], '856z' => e[7]}
 end
 
+holdings = []
 res.each do |e|
-  holdings = Holding.where(source_id: e['001']).where(lib_siglum: 'GB-Lbl').where('created_at > ?', Time.parse('2022-03-01'))
-  holdings.each do |holding|
-    puts holding.id
-    holding.marc.each_by_tag("852") do |tag|
-      if tag.indicator = ""
-        node = MarcNode.new("holding", "852", "", "##")
-        tag.children.reverse.each do |t|
-          node.add_at(t, 0)
-        end
-        tag.destroy_yourself
-        ip = holding.marc.get_insert_position("852")
-        holding.marc.root.children.insert(ip, node)
+  holdings << Holding.where(source_id: e['001']).where(lib_siglum: 'GB-Lbl').where('created_at > ?', Time.parse('2022-03-01')).where(wf_owner: 0).pluck(:id)
+end
+
+holdings.flatten.uniq.each do |e|
+  holding = Holding.find(e)
+  marc = MarcHolding.new(holding.marc_source)
+  marc.load_source(false)
+
+  marc.each_by_tag("852") do |tag|
+    if tag.indicator = ""
+      node = MarcNode.new("holding", "852", "", "##")
+      tag.children.reverse.each do |t|
+        node.add_at(t, 0)
       end
+      node.add(MarcNode.new(Source, "x", "30001581", nil))
+      node.sort_alphabetically
+      tag.destroy_yourself
+      ip = marc.get_insert_position("852")
+      marc.root.children.insert(ip, node)
     end
-    begin
-      holding.save
-    rescue => e
-      $stderr.puts"SplitHoldingRecords could not save holding record for #{source.id}"
-      $stderr.puts e.message.blue
-      next
-    end
+  end
+  begin
+    import_marc = MarcHolding.new(marc.to_marc)
+    import_marc.import
+    holding.marc = import_marc
+    holding.save
+  rescue => e
+    $stderr.puts"Record could not save holding record for #{holding.source_id}:#{holding.id}"
+    $stderr.puts e.message.blue
+    next
   end
 end
 
