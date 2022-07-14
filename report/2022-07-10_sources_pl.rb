7177ae29e91bd744b17f4d4ee335a7e34c0c4a0c
stephan <stephan@hirsch-online.com>
2022-07-14 12:28:10 +0200

    Archiving

---
+++ report/2022-07-10_sources_pl.rb
@@ -0,0 +1,24 @@
+dir = "#{Rails.root}/housekeeping/maintenance"
+numbers = File.read("#{dir}/2022-07-10_sources_pl.txt").split("\n")
+res = ["<?xml version=\"1.0\" encoding=\"UTF-8\"?>", "<marc:collection xmlns:marc=\"http://www.loc.gov/MARC21/slim\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://www.loc.gov/MARC21/slim http://www.loc.gov/standards/marcxml/schema/MARC21slim.xsd\">"]
+
+basket = []
+
+numbers.each do |num|
+  number = num.strip
+  if basket.include?(number)
+    puts "### DUPLICATE #{number}"
+    next
+  else
+    basket << number
+  end
+  puts number
+  begin
+    source = Source.find(number) 
+  rescue
+    binding.pry
+  end
+  res << source.marc.to_xml_record(nil, nil, true) rescue next
+end
+res << "</marc:collection>" 
+File.write("#{dir}/out.xml", res.join("\n"))
