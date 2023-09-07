7d5fcdabcc23243a958b7a5c2884d0cab147f8ef
stephan <stephan@hirsch-online.com>
2023-09-07 11:41:59 +0200

    Reducing 245

--- report/report_245.rb
+++ report/report_245.rb
@@ -3,7 +3,7 @@ require 'pry'
 require 'yaml'
 require 'csv'
 
-ofile = "#{Rails.root}/housekeeping/maintenance/report/report_245.csv"
+ofile = "#{Rails.root}/housekeeping/maintenance/report/report_245_v2.csv"
 
 pt = YAML.load_file('/home/dev/projects/muscat-maintenance/lib/protypes.yaml')
 subs = {}
@@ -12,33 +12,34 @@ pt.each do |k,v|
 end
 re = Regexp.union(subs.keys)
 
-#db = SQLite3::Database.new "pikado.db"
-#rows = db.execute <<-SQL
-#  create table records (id varchar(100), pikado320 text, marc245 text);
-#SQL
+bar = ProgressBar.new(737613)
 
-id = nil
-marc_title = nil
+res = {}
 
 CSV.open(ofile, "w") do |csv|
-  csv << ["ID", "PiKaDo320", "Muscat245"]
+  csv << ["ID", "PiKaDo320", "Muscat245", "CREATED_AT", "UPDATED_AT"]
   File.foreach("/home/dev/backup/tit.txt").each do | line |
     if line.start_with?("000")
-      id = line[3..-1].strip
-      source = Source.where(id: id.to_i).take
-      if source
-        marc_title = source.marc.first_occurance("245", "a").content rescue "---"
+      bar.increment!
+      res = {}
+      res[:id] = line[3..-1].strip.to_i
+      source_record = Source.where(id: res[:id]).take
+      if source_record
+        if source_record.versions.empty?
+          source = source_record.marc_source.split("\n")
+          res[:marc_title] = source.grep(/=245/).first.split("$a").last.strip.split("$c").first.gsub("m̅", "mm").gsub("n̅", "nn")
+        end
       else
-        marc_title = "MUSCAT RECORD MISSING"
+        res[:marc_title] = nil
       end
     else
-      if id && line.starts_with?("320")
-        pikado_title = line[3..-1].gsub(re, subs).gsub("ÎÆ", "φ").strip
-        if pikado_title[-3..-1] != marc_title[-3..-1]
-          #db.execute("INSERT INTO records (id, pikado320, marc245) VALUES (?, ?, ?)", [id, pikado_title, marc_title])
-          csv << [id, pikado_title, marc_title]
-          #res[id] << line[3..-1]
-          #puts "#{id} -> PiKaDo: #{pikado_title} <-> MARC: #{marc_title}"
+      if res[:marc_title] && line.starts_with?("320")
+        res[:pikado_title] = line[3..-1].gsub(re, subs).gsub("ÎÆ", "φ").strip
+        if res[:pikado_title][-3..-1] != res[:marc_title][-3..-1]
+          if res[:marc_title]# && (source.updated_at == source.created_at)
+            puts res[:id]
+            csv << [res[:id], res[:pikado_title], res[:marc_title]]
+          end
         end
       end
     end
