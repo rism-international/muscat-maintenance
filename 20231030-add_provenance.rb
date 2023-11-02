70c8f7713fa139ace01f02bb6b96ad73e4f1d232
stephan <stephan@hirsch-online.com>
2023-11-02 12:39:11 +0100

    Added fmo to PL-Wu

---
+++ 20231030-add_provenance.rb
@@ -0,0 +1,47 @@
+# encoding: UTF-8
+#
+puts "##################################################################################################"
+puts "####################   ISSUE: Change fmo to provenance                  ##########################"
+puts "#########################   Expected collection size: ca. 2000  ##################################"
+puts "##################################################################################################"
+puts ""
+
+require_relative "lib/maintenance"
+
+siglum = "PL-Wu"
+sources = Source.where(lib_siglum: siglum).where(record_type: 1) + Source.where(lib_siglum: siglum).where(record_type: 2).where(source_id: nil) + Holding.where(lib_siglum: siglum)
+maintenance = Muscat::Maintenance.new(sources)
+provenance = Institution.find(51007403)
+
+process = lambda { |record|
+  klass = record.class.to_s == "Source" ? "Source" : "Holding"
+  record_type = record.record_type if klass == "Source"
+  has_tag = false
+  marc = record.marc
+  marc.each_by_tag("710") do |tag|
+    zero_tag = tag.fetch_first_by_tag("0")
+    if zero_tag.content == provenance.id
+      has_tag = true
+    end
+  end
+  next if has_tag
+
+  new_marc = "Marc#{klass}".constantize.new(record.marc_source)
+  new_marc.load_source(false)
+  ip = new_marc.get_insert_position("710")
+  new_710 = MarcNode.new("#{klass}".constantize, "710", "", "1#")
+  new_710.add(MarcNode.new("#{klass}".constantize, "0", provenance.id, nil))
+  new_710.add(MarcNode.new("#{klass}".constantize, "4", "fmo", nil))
+  new_marc.root.children.insert(ip, new_710)
+  
+  import_marc = "Marc#{klass}".constantize.new(new_marc.to_marc)
+  import_marc.load_source(false)
+  import_marc.import
+  record.marc = import_marc
+  record.record_type = record_type if klass == "Source"
+  maintenance.logger.info("#{maintenance.host}: #{klass} ##{record.id} $710 added fmo #{siglum}.")
+  record.save!
+}
+
+maintenance.execute process
+
--- report/2022-07-10_sources_pl.rb
+++ report/2022-07-10_sources_pl.rb
@@ -1,5 +1,5 @@
 dir = "#{Rails.root}/housekeeping/maintenance"
-numbers = File.read("#{dir}/2022-07-10_sources_pl.txt").split("\n")
+numbers = File.read("#{dir}/plsa.txt").split("\n")
 res = ["<?xml version=\"1.0\" encoding=\"UTF-8\"?>", "<marc:collection xmlns:marc=\"http://www.loc.gov/MARC21/slim\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://www.loc.gov/MARC21/slim http://www.loc.gov/standards/marcxml/schema/MARC21slim.xsd\">"]
 
 basket = []
@@ -10,15 +10,15 @@ numbers.each do |num|
     puts "### DUPLICATE #{number}"
     next
   else
-    basket << number
+    basket << number.to_i
   end
   puts number
   begin
-    source = Source.find(number) 
+    source = Source.find(number)
   rescue
     binding.pry
   end
   res << source.marc.to_xml_record(nil, nil, true) rescue next
 end
 res << "</marc:collection>" 
-File.write("#{dir}/out.xml", res.join("\n"))
+File.write("#{dir}/plsa.xml", res.join("\n"))
