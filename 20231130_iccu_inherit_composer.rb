dd40b3c3dbce06a9bd49bd40a5af22ffc9b8dcf1
stephan <stephan@hirsch-online.com>
2024-02-29 10:16:22 +0100

    Inherit composer with ICCU from folder

--- 20231130_iccu_inherit_composer.rb
+++ 20231130_iccu_inherit_composer.rb
@@ -1,6 +1,6 @@
 # encoding: UTF-8
 puts "##################################################################################################"
-puts "###########################  ISSUE : Change Siglum for ICCU     ##################################"
+puts "###########################  ISSUE : Change Composer for ICCU   ##################################"
 puts "############################   Expected collection size: 9.000  ##################################"
 puts "########################    Inherit siglum from collection       #################################"
 puts "##################################################################################################"
@@ -8,24 +8,22 @@ puts ""
 
 require_relative "lib/maintenance"
 
+sources = Folder.find(1102).folder_items.map{|e| e.item}
+maintenance = Muscat::Maintenance.new(sources)
 
-#sources = Source.where(wf_owner: 268).where(record_type: 1)
-
-#maintenance = Muscat::Maintenance.new(sources)
-
-#process = lambda { |record|
-#  shelfmark = record.marc.get_siglum_and_shelf_mark[1]
-#  next if shelfmark == "[no indication]"
-#
-#  record.child_sources.each do | child |
-#    child_shelfmark = child.marc.get_siglum_and_shelf_mark[1]
-#    next if child_shelfmark != "[no indication]"
-#    node = child.marc.first_occurance("852", "c")
-#    node.content = shelfmark
-#    child.save!
-#    maintenance.logger.info("#{maintenance.host}: #{child.id} shelfmark '[no indication]' -> '#{shelfmark}'")
-#  end
-#}
+process = lambda { |record|
+  new_composer = record.marc.first_occurance("100")
+  children = Source.where(source_id: record.id)
+  children.each do |child|
+    marc = child.marc
+    old_composer = marc.first_occurance("100")
+    old_composer.destroy_yourself
+    ip = marc.get_insert_position("100")
+    marc.root.children.insert(ip, new_composer)
+    child.save
+    maintenance.logger.info("#{maintenance.host}: #{child.id} composer -> '#{new_composer.to_s}'")
+  end
+}
 
 maintenance.execute process
 
