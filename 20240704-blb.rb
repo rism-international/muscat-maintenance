979fa340eba899835a472bb64958c3357ab2fefb
stephan <stephan@hirsch-online.com>
2024-07-08 11:59:16 +0200

    BLB urls

---
+++ 20240704-blb.rb
@@ -0,0 +1,41 @@
+# encoding: UTF-8
+# #
+# puts "##################################################################################################"
+# puts "########################    ISSUE: Add Digits to BLB             #################################"
+# puts "#########################   Expected collection size: 10.000    ##################################"
+# puts "##################################################################################################"
+# puts ""
+#
+require_relative "lib/maintenance"
+
+filename = "#{File.dirname($0)}/#{File.basename($0, '.rb')}.csv"
+res = {}
+
+CSV.foreach(filename, :headers => true) do |row|
+  res[row[0].to_i] = row[1].strip 
+end
+
+sources = Source.where(:id => res.keys)
+maintenance = Muscat::Maintenance.new(sources)
+   
+
+process = lambda { |record|
+     modified = false
+     url = res[record.id]
+     if record.marc_source.include?(url.split('://').last)
+       next
+     end
+     marc = record.marc
+     new_856 = MarcNode.new(Source, "856", "", "4#")
+     ip = marc.get_insert_position("856")
+     new_856.add(MarcNode.new(Source, "u", "#{url}", nil))
+     new_856.add(MarcNode.new(Source, "x", "Digitized", nil))
+     new_856.add(MarcNode.new(Source, "z", "Digital copy", nil))
+     new_856.sort_alphabetically
+     marc.root.children.insert(ip, new_856)
+     maintenance.logger.info("#{maintenance.host}: Source ##{record.id} new digitizalization with content '#{url}'")
+     modified = true
+     record.save if modified
+}
+
+maintenance.execute process
