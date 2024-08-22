6b35d2e880e39976b11ddbea18102562e0f01a19
stephan <stephan@hirsch-online.com>
2024-08-22 10:33:15 +0200

    Hyperlinks

--- report/20240820-check-588.rb
+++ report/20240820-check-588.rb
@@ -22,7 +22,7 @@ sources.each do |source|
       end
     end
     if containing == false
-      res << ["<a href=\"https://muscat.rism.info/admin/sources/#{source.id}\">#{source.id}</a>", st, holdings.join("; ")]
+      res << ["=HYPERLINK(\"\"https://muscat.rism.info/admin/sources/#{source.id}\"\", \"\"#{source.id}\"\")", st, holdings.join("; ")]
     end
   end
 end
