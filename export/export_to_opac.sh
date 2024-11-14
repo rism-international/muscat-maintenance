e3de24d50935447241e8fbd13b99073c36e6e205
stephan <stephan@hirsch-online.com>
2024-11-14 11:58:48 +0100

    Modified export script

--- export/export_to_opac.sh
+++ export/export_to_opac.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 cd ../../
-rails runner housekeeping/maintenance/export/sources_to_bsb.rb
-rails runner housekeeping/maintenance/export/people_to_bsb.rb
-rails runner housekeeping/maintenance/export/institutions_to_bsb.rb
-rails runner housekeeping/maintenance/export/catalogues_to_bsb.rb
+rails r housekeeping/export/xml-export.rb -f /tmp/sources.xml -m Source -l
+rails r housekeeping/export/xml-export.rb -f /tmp/people.xml -m Person -l
+rails r housekeeping/export/xml-export.rb -f /tmp/institutions.xml -m Institution -l
+rails r housekeeping/export/xml-export.rb -f /tmp/catalogues.xml -m Publication -l
+
+
+
+#rails runner housekeeping/maintenance/export/sources_to_bsb.rb
+#rails runner housekeeping/maintenance/export/people_to_bsb.rb
+#rails runner housekeeping/maintenance/export/institutions_to_bsb.rb
+#rails runner housekeeping/maintenance/export/catalogues_to_bsb.rb
 #rails runner housekeeping/maintenance/export/works_to_bsb.rb
 #rails runner housekeeping/maintenance/export/generate_beacon.rb
